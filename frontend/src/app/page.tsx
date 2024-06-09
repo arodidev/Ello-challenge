7d033b9f135aba540dbbfb4d46624e874af4e550
arodidev <51090527+arodidev@users.noreply.github.com>
2024-06-09 20:56:45 +0300

    cleanup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,20 +5,14 @@ import BookAssignmentView from "../components/BookAssignmentView";
 import { ThemeProvider } from "@mui/system";
 import theme from "src/themeProvider";
 
-//revamp UI: Done
-//change font: Done
-//TODO: ensure reading list can only be populated with one item at a time: Done
 //enhance error handling for all components and fetch calls TO:DO
 //change fetch to use SWR
 //add tests
-//add loading states: Done
-//add empty states: Done
-//rename files especially root component
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BookAssignmentView />;
+      <BookAssignmentView />
     </ThemeProvider>
   );
 }
